f587bc6c58a139498db23b63041d3edd6260f093
alion <alionwithfield@protonmail.ch>
2022-03-30 14:35:04 +0200

    Removed bootstrap added vite

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# portfolio-manager-
+scaffolded with Vite (no vue-cli)
+eslint and prettier for formatting and linting
+tailwind (no bootstrap)
+volar in VsCode (instead of Vetur)
+
--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
-import BootstrapVue from 'bootstrap-vue';
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import 'bootstrap/dist/css/bootstrap.css';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-Vue.use(BootstrapVue);
+import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false;
+const app = createApp(App)
 
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount('#app');
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
